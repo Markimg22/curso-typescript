1dd6a5c73fd54b3ebf8b55a56c2986f24d23aca5
Markimg22 <marquim.net.221200@hotmail.com>
2021-01-13 16:32:50 -0400

    decorator parameter

---
+++ src/aulas/aula_51/decorator-parameter.ts
@@ -0,0 +1,56 @@
+/** Decorator Parameter:
+ *
+ * Só funciona para assitir o parâmetro, saber oque ele é, e o que tem.
+ */
+
+function decorator(
+  classPrototype: any,
+  nameMethod: string,
+  descriptor: PropertyDescriptor,
+): PropertyDescriptor | void {
+  console.log(classPrototype);
+  console.log(nameMethod);
+  console.log(descriptor);
+
+  return {
+    value: function (...args: string[]) {
+      return args[0].toLocaleUpperCase();
+    },
+  };
+}
+
+export class OnePeople {
+  name: string;
+  lastName: string;
+  age: number;
+
+  constructor(name: string, lastName: string, age: number) {
+    this.name = name;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  @decorator
+  method(msg: string): string {
+    return `${this.name} ${this.lastName}: ${msg}`;
+  }
+
+  get fullName(): string {
+    return this.name + ' ' + this.lastName;
+  }
+
+  set fullName(value: string) {
+    const words = value.split(/\s+/g);
+    const firstName = words.shift();
+
+    if (!firstName) return;
+
+    this.name = firstName;
+    this.lastName = words.join(' ');
+  }
+}
+
+const people = new OnePeople('Marcos', 'Campos', 20);
+const method = people.method('Hello world');
+
+console.log(method);
